c6441f3a615619f63bffb5e205b54645338d8837
vmedrano14c <141966048+vmedrano14c@users.noreply.github.com>
2023-10-10 11:15:35 -0500

    intento 2
    creación de la clase tesis

---
+++ src/main/java/comunicacion/Tesis.java
@@ -0,0 +1,77 @@
+package comunicacion;
+
+public class Tesis extends Escrito {
+	private String idea;
+	private String[] argumentos = new String[] {};
+	private String conclusion;
+	private String referencias;
+	private String interpretacion;
+	
+	public Tesis(String origen, String titulo, String autor, int paginas, String idea, String[] argumentos, 
+			String conclusion, String referencias, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.idea = idea;
+		this.argumentos = (argumentos != null) ? argumentos : this.argumentos;
+		this.conclusion = conclusion;
+		this.referencias = referencias;
+		this.interpretacion = interpretacion;
+	}
+
+	@Override
+	public int palabrasTotales(int factor) {
+		return this.getPaginas() * factor * 5;
+	}
+
+	@Override
+	public String interpretacion() {
+		return this.interpretacion;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s\n%s\n%d\n%s\n%s", this.resumen(), this.idea, this.argumentos.length,
+				this.conclusion, this.referencias);
+	}
+
+	public String getIdea() {
+		return idea;
+	}
+
+	public void setIdea(String idea) {
+		this.idea = idea;
+	}
+
+	public String[] getArgumentos() {
+		return argumentos;
+	}
+
+	public void setArgumentos(String[] argumentos) {
+		this.argumentos = argumentos;
+	}
+
+	public String getConclusion() {
+		return conclusion;
+	}
+
+	public void setConclusion(String conclusion) {
+		this.conclusion = conclusion;
+	}
+
+	public String getReferencias() {
+		return referencias;
+	}
+
+	public void setReferencias(String referencias) {
+		this.referencias = referencias;
+	}
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	
+}
